2b726d0959c7c100413ef8711225d214c4068390
Marcio Camello <marciocamello@outlook.com>
2020-10-15 18:02:14 -0300

    finish part one

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function OrphanageDetails() {
+    return (
+        <View />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
